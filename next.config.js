c0137d816a046d99b84f1c324d9b7e82ce4e580f
Jett <jd.dev96@gmail.com>
2022-12-22 21:03:09 -0500

    added anilist domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["media.kitsu.io"],
+    domains: ["media.kitsu.io", "anilist.co"],
     // formats: ["image/avif", "image/webp"],
     // remotePatterns: [
     //   {
--- pages/anilist/index.tsx
+++ pages/anilist/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const AniList = () => {
@@ -17,46 +18,110 @@ const AniList = () => {
           </div>
         </ul>
       </nav>
-      <header className="mx-auto my-12 bg-gray-800 px-20 py-16 max-w-6xl rounded-3xl">
-        <h1 className="text-3xl font-bold text-gray-50 mb-10 mx-auto max-w-full text-center">
+      <header className="mx-auto my-12 bg-gray-800 px-20 py-16 max-w-6xl rounded-3xl shadow-2xl">
+        <h1 className="text-3xl font-bold text-gray-100 mb-10 mx-auto max-w-full text-center">
           The next generation anime platform
         </h1>
-        <h2 className="max-w-full text-xl text-center text-emerald-100">
+        <h2 className="max-w-full text-xl text-center text-emerald-200">
           Track, share, and discover your favorite anime and manga with
           YourAnime😍
         </h2>
         <div className="grid grid-cols-2 mx-auto my-24 gap-y-20 gap-x-16">
-          <div className="grid">
+          <div
+            className="grid grid-cols-2"
+            style={{
+              display: "grid",
+              gridTemplateColumns: "80px auto",
+              gridGap: "40px",
+            }}
+          >
+            <Image
+              width={100}
+              height={100}
+              src="https://anilist.co/img/landing/stats.svg"
+              alt="pic"
+              role="presentation"
+            />
             <div>
-              <h3>Discover your obsessions</h3>
-              <p>
+              <h3 className="text-lg font-semibold text-gray-100">
+                Discover your obsessions<span className="ml-2">🔎</span>
+              </h3>
+              <p className="text-md text-emerald-200">
                 What are your highest rated genres or most watched voice actors?
                 Follow your watching habits over time with in-depth statistics.
               </p>
             </div>
           </div>
-          <div className="grid">
+          <div
+            className="grid grid-cols-2"
+            style={{
+              display: "grid",
+              gridTemplateColumns: "80px auto",
+              gridGap: "40px",
+            }}
+          >
+            <Image
+              width={100}
+              height={100}
+              src="	https://anilist.co/img/landing/apps.svg"
+              alt="pic"
+              role="presentation"
+            />
             <div>
-              <h3>Discover your obsessions</h3>
-              <p>
+              <h3 className="text-lg font-semibold text-gray-100">
+                Bring YourAnime anywhere <span className="ml-2">🌎</span>
+              </h3>
+              <p className="text-md text-emerald-200">
                 What are your highest rated genres or most watched voice actors?
                 Follow your watching habits over time with in-depth statistics.
               </p>
             </div>
           </div>
-          <div className="grid">
+          <div
+            className="grid grid-cols-2"
+            style={{
+              display: "grid",
+              gridTemplateColumns: "80px auto",
+              gridGap: "40px",
+            }}
+          >
+            <Image
+              width={100}
+              height={100}
+              src="https://anilist.co/img/landing/social.svg"
+              alt="pic"
+              role="presentation"
+            />
             <div>
-              <h3>Discover your obsessions</h3>
-              <p>
+              <h3 className="text-lg font-semibold text-gray-100">
+                Join the conversation <span className="ml-2"> 📝</span>
+              </h3>
+              <p className="text-md text-emerald-200">
                 What are your highest rated genres or most watched voice actors?
                 Follow your watching habits over time with in-depth statistics.
               </p>
             </div>
           </div>
-          <div className="grid">
+          <div
+            className="grid grid-cols-2"
+            style={{
+              display: "grid",
+              gridTemplateColumns: "80px auto",
+              gridGap: "40px",
+            }}
+          >
+            <Image
+              width={100}
+              height={100}
+              src="	https://anilist.co/img/landing/custom.svg"
+              alt="pic"
+              role="presentation"
+            />
             <div>
-              <h3>Discover your obsessions</h3>
-              <p>
+              <h3 className="text-lg font-semibold text-gray-100">
+                Tweak it to your liking <span className="ml-2">💕</span>
+              </h3>
+              <p className="text-md text-emerald-200">
                 What are your highest rated genres or most watched voice actors?
                 Follow your watching habits over time with in-depth statistics.
               </p>
