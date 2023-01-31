9b09b66cc2b0694bcfc310bd9eb83c1d77d1bbd8
Jett <jd.dev96@gmail.com>
2023-01-30 20:28:37 -0500

    installed graphql with apollo

--- pages/anilist/index.tsx
+++ pages/anilist/index.tsx
@@ -117,12 +117,12 @@ const AniList = () => {
 				<ul className="flex gap-60 text-gray-100">
 					<li className="my-auto font-medium">AL</li>
 					<div className="flex gap-10">
-						<li className="my-auto font-medium">Search</li>
-						<li className="my-auto font-medium">Social</li>
-						<li className="my-auto font-medium">Forum</li>
+						<li className="my-auto font-medium cursor-pointer">Search</li>
+						<li className="my-auto font-medium cursor-pointer">Social</li>
+						<li className="my-auto font-medium cursor-pointer">Forum</li>
 					</div>
 					<div className="flex gap-4">
-						<li className="my-auto font-medium">Login</li>
+						<li className="my-auto font-medium cursor-pointer">Login</li>
 						<li>
 							<button className="rounded-md bg-emerald-500 text-white px-3 py-1 font-medium">
 								Sign Up
