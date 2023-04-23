8a09cdbb0d8b767bc176d91ac06d73d808ba46c1
Jett <jd.dev96@gmail.com>
2023-04-23 12:59:41 -0400

    nextauth sign working

---
+++ src/components/Elements/Login.tsx
@@ -0,0 +1,17 @@
+"use client";
+"use client";
+
+import { signIn } from "next-auth/react";
+
+export default function Login() {
+	return (
+		<li
+			className="my-auto font-medium cursor-pointer"
+			onClick={() => {
+				signIn();
+			}}
+		>
+			Login
+		</li>
+	);
+}
--- src/components/Layout/Nav.tsx
+++ src/components/Layout/Nav.tsx
@@ -1,4 +1,11 @@
-export default function Nav() {
+import Link from "next/link";
+import Login from "../Elements/Login";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
+
+export default async function Nav() {
+	const session = await getServerSession(authOptions);
+	console.log(session);
 	return (
 		<ul className="flex gap-60 text-gray-100">
 			<li className="my-auto font-medium">AL</li>
@@ -8,7 +15,7 @@ export default function Nav() {
 				<li className="my-auto font-medium cursor-pointer">Forum</li>
 			</div>
 			<div className="flex gap-4">
-				<li className="my-auto font-medium cursor-pointer">Login</li>
+				<Login />
 				<li>
 					<button className="rounded-md bg-emerald-500 text-white px-3 py-1 font-medium">
 						Sign Up
