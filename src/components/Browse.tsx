2f28bba38f7e9a4e36a7c71d23b5d44d8e7de681
Jett <jd.dev96@gmail.com>
2023-04-22 21:25:08 -0400

    styling added to homepage components

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -51,7 +51,7 @@ export default function Browse() {
 				<div className="flex"></div>
 			</div>
 
-			<section className="grid sm:grid-cols-2 md:grid-cols-4 2xl:grid-cols-6 3xl:grid-cols-8 gap-4">
+			{/* <section className="grid sm:grid-cols-2 md:grid-cols-4 2xl:grid-cols-6 3xl:grid-cols-8 gap-4">
 				{data.Trending.media.map((media: Media, i: number) => (
 					<div className="flex overflow-y-scroll overflow-x-hidden" key={i}>
 						<a
@@ -99,6 +99,26 @@ export default function Browse() {
 								</div>
 							</div>
 						</a>
+					</div>
+				))}
+			</section> */}
+			<section className="grid sm:grid-cols-2 md:grid-cols-4 2xl:grid-cols-6 3xl:grid-cols-8 gap-4">
+				{data.Trending.media.map((media: Media, i: number) => (
+					<div
+						key={media.id}
+						className="relative w-full h-48 bg-gray-700 rounded-md overflow-hidden group"
+					>
+						<img
+							src={media.coverImage.large}
+							alt={media.title.english || media.title.native}
+							className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-110"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t from-black opacity-40 transition-opacity duration-300 ease-in-out group-hover:opacity-0"></div>
+						<div className="absolute bottom-0 left-0 w-full p-2 text-white transition-all duration-300 ease-in-out group-hover:bottom-2">
+							<h3 className="text-sm font-semibold">
+								{media.title.english || media.title.native}
+							</h3>
+						</div>
 					</div>
 				))}
 			</section>
--- src/components/PopularAnime.tsx
+++ src/components/PopularAnime.tsx
@@ -33,14 +33,35 @@ export default function PopularAnime() {
 	}
 
 	return (
+		// <section className="grid sm:grid-cols-2 md:grid-cols-4 2xl:grid-cols-6 3xl:grid-cols-8 gap-4">
+		// 	{data.Page.media.map((anime) => (
+		// 		<div key={anime.id} className="relative">
+		// 			<h3 className="mb-2">{anime.title.english || anime.title.native}</h3>
+		// 			<img
+		// 				src={anime.coverImage.large}
+		// 				alt={anime.title.english || anime.title.native}
+		// 				className="transition duration-200 ease-in-out transform hover:scale-105 hover:shadow-lg"
+		// 			/>
+		// 		</div>
+		// 	))}
+		// </section>
 		<section className="grid sm:grid-cols-2 md:grid-cols-4 2xl:grid-cols-6 3xl:grid-cols-8 gap-4">
 			{data.Page.media.map((anime) => (
-				<div key={anime.id}>
-					<h3>{anime.title.english || anime.title.native}</h3>
+				<div
+					key={anime.id}
+					className="relative w-full h-48 bg-gray-700 rounded-md overflow-hidden group hover:cursor-pointer"
+				>
 					<img
 						src={anime.coverImage.large}
 						alt={anime.title.english || anime.title.native}
+						className="w-full h-full object-cover transition duration-300 ease-in-out transform group-hover:scale-110 "
 					/>
+					<div className="absolute inset-0 bg-gradient-to-t from-black opacity-40 transition-opacity duration-300 ease-in-out group-hover:opacity-0"></div>
+					<div className="absolute bottom-0 left-0 w-full p-2 text-white transition-all duration-300 ease-in-out group-hover:bottom-2">
+						<h3 className="text-sm font-semibold">
+							{anime.title.english || anime.title.native}
+						</h3>
+					</div>
 				</div>
 			))}
 		</section>
