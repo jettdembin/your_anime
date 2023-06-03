1eb984eaf1c1a8698b3f370ea5ccadde3abc3e0a
Jett <jd.dev96@gmail.com>
2023-06-03 15:23:01 -0400

    created loginwrapper to wrap join now button and login button in nav header

---
+++ src/components/Elements/LoginWrapper.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState, useRef } from "react";
+
+import { SignUp } from "@clerk/clerk-react";
+import { signIn } from "next-auth/react";
+
+import useClickOutside from "@/src/hooks/useClickOutside";
+
+const LoginWrapper = ({ children }) => {
+	const [isSigningIn, setIsSigningIn] = useState(false);
+	const signUpFormRef = useRef(null);
+
+	const toggleSignUpForm = () => {
+		setIsSigningIn(!isSigningIn);
+	};
+
+	useClickOutside(signUpFormRef, toggleSignUpForm);
+
+	return (
+		<div
+			onClick={() => {
+				setIsSigningIn(true);
+			}}
+		>
+			{children}
+			{isSigningIn && (
+				<div className="overflow-hidden">
+					<div className="fixed w-screen h-screen top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-600 z-10 opacity-50"></div>
+					<div className="fixed w-screen h-screen z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  flex items-center justify-center">
+						<div ref={signUpFormRef}>
+							<SignUp />
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default LoginWrapper;
--- src/components/Pages/Home/Header.tsx
+++ src/components/Pages/Home/Header.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
+import LoginWrapper from "../../Elements/LoginWrapper";
+
+// import LoginWrapper from "@/src/app/components/Elements/LoginWrapper";
+
 export default function HomeHeader() {
 	return (
 		<>
@@ -112,31 +116,33 @@ export default function HomeHeader() {
 					</div>
 				</div>
 			</div>
-			<div className="absolute -bottom-8 left-1/2 -translate-x-1/2">
-				<div
-					// href="/signup"
-					className="flex w-72 items-center justify-around pl-14  py-4 text-white bg-blue-500 rounded-full hover:bg-blue-600 transition duration-200"
-				>
-					<div className="text-2xl font-bold tracking-wider">Join Now</div>
-					<div className="flex items-center justify-center bg-white text-blue-500 rounded-full w-10 h-10">
-						<svg
-							aria-hidden="true"
-							focusable="false"
-							data-prefix="fas"
-							data-icon="chevron-right"
-							role="img"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 320 512"
-							className="w-4 h-4 fill-current"
-						>
-							<path
-								fill="currentColor"
-								d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
-							/>
-						</svg>
+			<LoginWrapper>
+				<div className="absolute -bottom-8 left-1/2 -translate-x-1/2">
+					<div
+						// href="/signup"
+						className="flex w-72 items-center justify-around pl-14  py-4 text-white bg-blue-500 rounded-full hover:bg-blue-600 transition duration-200"
+					>
+						<div className="text-2xl font-bold tracking-wider">Join Now</div>
+						<div className="flex items-center justify-center bg-white text-blue-500 rounded-full w-10 h-10">
+							<svg
+								aria-hidden="true"
+								focusable="false"
+								data-prefix="fas"
+								data-icon="chevron-right"
+								role="img"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 320 512"
+								className="w-4 h-4 fill-current"
+							>
+								<path
+									fill="currentColor"
+									d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+								/>
+							</svg>
+						</div>
 					</div>
 				</div>
-			</div>
+			</LoginWrapper>
 		</>
 	);
 }
