6be92e396790eeda8ec11ee98e1dd5af618f7f13
Jett <jd.dev96@gmail.com>
2023-10-26 21:10:21 -0400

    nac fixed

--- src/components/Layout/Nav.tsx
+++ src/components/Layout/Nav.tsx
@@ -1,8 +1,41 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Login from "../Elements/Login";
 
 export default function Nav() {
+	const [lastScrollTop, setLastScrollTop] = useState(0);
+	const [navClass, setNavClass] = useState(
+		"transform translate-y-0 transition-transform duration-300"
+	);
+
+	useEffect(() => {
+		function handleScroll() {
+			let st = window.pageYOffset || document.documentElement.scrollTop;
+			if (st > lastScrollTop) {
+				// downscroll code
+				setNavClass(
+					"transform -translate-y-full transition-transform duration-300"
+				);
+			} else {
+				// upscroll code
+				setNavClass(
+					"transform translate-y-0 transition-transform duration-300"
+				);
+			}
+			setLastScrollTop(st <= 0 ? 0 : st);
+		}
+
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, [lastScrollTop]);
+
 	return (
-		<nav className="hidden lg:flex h-20 px-8 bg-gray-800 items-center justify-center sticky top-0 z-50">
+		<nav
+			className={`lg:flex w-full p-8 bg-gray-800 items-center justify-center fixed top-0 z-50 ${navClass} opacity-90`}
+		>
 			<ul className="flex gap-60 text-gray-100">
 				<li className="my-auto font-medium">YAnime</li>
 				<div className="flex gap-10">
