24f86313f0f4a7e4f8e626f98b3ecece745dd84b
Jett Dembin <premiumconsulting@jettdembin.dev>
2023-11-23 13:18:30 -0500

    radix button anime started

--- components/Pages/Home/ui/AnimeCard/ListType/AnimeHoverOptions.jsx
+++ components/Pages/Home/ui/AnimeCard/ListType/AnimeHoverOptions.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-import { Select } from "@radix-ui/react-select";
+import { Flex, Text, Button } from "@radix-ui/themes";
 
 import * as HoverCard from "@radix-ui/react-hover-card";
 
@@ -12,7 +12,7 @@ const AnimeHoverOptions = () => {
   return (
     <HoverCard.Root>
       <HoverCard.Trigger asChild>
-        <span className="material-icons cursor-pointer text-blue-100 relative">
+        <span className="material-icons cursor-pointer text-black relative">
           loupe
           {/* <AnimatePresence>
 						{isAnimeHoverOptionsHovered && (
@@ -33,169 +33,122 @@ const AnimeHoverOptions = () => {
         <HoverCard.Content
           data-side="bottom"
           side="top"
-          className="HoverCardContent z-50"
+          className="HoverCardContent z-50 shadow-xl"
           sideOffset={5}
         >
-          {/* <div style={{ display: "flex", flexDirection: "column", gap: 7 }}>
-						<img
-							className="Image large"
-							src="https://pbs.twimg.com/profile_images/1337055608613253126/r_eiMp2H_400x400.png"
-							alt="Radix UI"
-						/>
-						<div style={{ display: "flex", flexDirection: "column", gap: 15 }}>
-							<div>
-								<div className="Text bold">Radix</div>
-								<div className="Text faded">@radix_ui</div>
-							</div>
-							<div className="Text">
-								Components, icons, colors, and templates for building
-								high-quality, accessible UI. Free and open-source.
-							</div>
-							<div style={{ display: "flex", gap: 15 }}>
-								<div style={{ display: "flex", gap: 5 }}>
-									<div className="Text bold">0</div>{" "}
-									<div className="Text faded">Following</div>
-								</div>
-								<div style={{ display: "flex", gap: 5 }}>
-									<div className="Text bold">2,900</div>{" "}
-									<div className="Text faded">Followers</div>
-								</div>
-							</div>
-						</div>
-					</div> */}
-          {/* <div className="w-full text-black">
-						<div
-							className={`flex gap-2 ${status === "watching" ? "active" : ""}`}
-							onClick={() => setStatus("watching")}
-						>
-							<span className="material-icons">visibility</span>
-							<span>Completed</span>
-						</div>
-						<div
-							className={`flex gap-2 ${status === "maybe" ? "active" : ""}`}
-							onClick={() => setStatus("maybe")}
-						>
-							<span className="material-icons">help_outline</span>
-							<span>Watching</span>
-						</div>
-						<div
-							className={`flex gap-2 ${status === "maybe" ? "active" : ""}`}
-							onClick={() => setStatus("maybe")}
-						>
-							<span className="material-icons">help_outline</span>
-							<span>Considering</span>
-						</div>
-						<div
-							className={`flex gap-2 ${
-								status === "notwatching" ? "active" : ""
-							}`}
-							onClick={() => setStatus("notwatching")}
-						>
-							<span className="material-icons">visibility_off</span>
-							<span>Dropped</span>
-						</div>
-					</div> */}
-          <div className="grid grid-cols-2 gap-2 bg-white p-4 m-auto rounded-sm">
-            <div>
-              <label
-                for="library_editor_statusSelect"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Status
-              </label>
-              <select value={status} onValueChange={setStatus}>
-                <option value="completed">Completed</option>
-                <option value="rewatching">Rewatching</option>
-                <option value="watching">Watching</option>
-                <option value="planning">Planning</option>
-                <option value="considering">Considering</option>
-                <option value="paused">Paused</option>
-                <option value="dropped">Dropped</option>
-                <option value="skipping">Skipping</option>
-              </select>
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                Rating
-              </label>
-              <select className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
-                <option value="10">10 (Masterpiece)</option>
-                <option value="9">9 (Incredible)</option>
-                <option value="8">8 (Great)</option>
-                <option value="7">7 (Good)</option>
-                <option value="6">6 (Okay)</option>
-                <option value="5">5 (Mediocre)</option>
-                <option value="4">4 (Poor)</option>
-                <option value="3">3 (Bad)</option>
-                <option value="2">2 (Awful)</option>
-                <option value="1">1 (Atrocious)</option>
-                <option value="">–</option>
-              </select>
-            </div>
+          <div className=" bg-white p-4 m-auto rounded-md">
+            <form action="">
+              <div className="grid grid-cols-2 gap-2">
+                <div>
+                  <label
+                    for="library_editor_statusSelect"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    Status
+                  </label>
+                  <select value={status} onValueChange={setStatus}>
+                    <option value="WATCHED">Completed</option>
+                    <option value="">Rewatching</option>
+                    <option value="WATCHING">Watching</option>
+                    <option value="TO_WATCH">Planning</option>
+                    <option value="considering">Considering</option>
+                    <option value="paused">Paused</option>
+                    <option value="DROPPED">Dropped</option>
+                    <option value="skipping">Skipping</option>
+                  </select>
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Rating
+                  </label>
+                  <select className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+                    <option value="10">10 (Masterpiece)</option>
+                    <option value="9">9 (Incredible)</option>
+                    <option value="8">8 (Great)</option>
+                    <option value="7">7 (Good)</option>
+                    <option value="6">6 (Okay)</option>
+                    <option value="5">5 (Mediocre)</option>
+                    <option value="4">4 (Poor)</option>
+                    <option value="3">3 (Bad)</option>
+                    <option value="2">2 (Awful)</option>
+                    <option value="1">1 (Atrocious)</option>
+                    <option value="">–</option>
+                  </select>
+                </div>
 
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                Progress
-              </label>
-              <input
-                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                name="rewatches"
-                type="number"
-                min="0"
-                max="999"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                Full Rewatches
-              </label>
-              <input
-                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                name="rewatches"
-                type="number"
-                min="0"
-                max="999"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                Started
-              </label>
-              <input
-                id="library_editor_startedAt"
-                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                name="started_at"
-                type="date"
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-gray-700">
-                Finished
-              </label>
-              <input
-                id="library_editor_finishedAt"
-                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                name="finished_at"
-                type="date"
-              />
-            </div>
-            <div className="col-span-full">
-              <label className="block text-sm font-medium text-gray-700">
-                Personal Notes
-              </label>
-              <textarea
-                id="library_editor_notes"
-                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                name="notes"
-                rows="6"
-              ></textarea>
-              <small className="text-secondary">
-                <span>Only visible to you</span>
-                <span className="float-right">500 / 500</span>
-              </small>
-            </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Progress
+                  </label>
+                  <input
+                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                    name="rewatches"
+                    type="number"
+                    min="0"
+                    max="999"
+                    required
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Full Rewatches
+                  </label>
+                  <input
+                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                    name="rewatches"
+                    type="number"
+                    min="0"
+                    max="999"
+                    required
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Started
+                  </label>
+                  <input
+                    id="library_editor_startedAt"
+                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                    name="started_at"
+                    type="date"
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-700">
+                    Finished
+                  </label>
+                  <input
+                    id="library_editor_finishedAt"
+                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                    name="finished_at"
+                    type="date"
+                  />
+                </div>
+                <div className="col-span-full">
+                  <label className="block text-sm font-medium text-gray-700">
+                    Personal Notes
+                  </label>
+                  <textarea
+                    id="library_editor_notes"
+                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                    name="notes"
+                    rows="6"
+                  ></textarea>
+                  <small className="text-secondary">
+                    <span>Only visible to you</span>
+                    <span className="float-right">500 / 500</span>
+                  </small>
+                </div>
+              </div>
+              <div className="flex justify-end">
+                <div className="cursor-pointer">
+                  <Button variant="outline" color="blue">
+                    <div className="py-1 px-2">Submit</div>
+                    <span className="material-icons">chevron_right</span>
+                  </Button>
+                </div>
+              </div>
+            </form>
           </div>
           <HoverCard.Arrow className="HoverCardArrow" />
         </HoverCard.Content>
