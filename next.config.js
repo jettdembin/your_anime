3033472c7bc281278ea4fe56da2d1d075283de08
Jett Dembin <premiumconsulting@jettdembin.dev>
2023-12-08 17:55:42 -0500

    chjange configs

--- next.config.js
+++ next.config.js
@@ -1,17 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // presets: ["next/babel"],
-  // plugins: ["inline-react-svg"],
+  presets: ["next/babel"],
+  plugins: ["inline-react-svg"],
+  experimental: {
+    appDir: true,
+  },
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "media.kitsu.io" },
-      { protocol: "https", hostname: "anilist.co" },
-      { protocol: "https", hostname: "s4.anilist.co" },
-      { protocol: "https", hostname: "i.ytimg.com" },
-    ],
+    domains: ["media.kitsu.io", "anilist.co", "s4.anilist.co", "i.ytimg.com"],
     // formats: ["image/avif", "image/webp"],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "assets.vercel.com",
+    //     port: "",
+    //     pathname: "/image/upload/**",
+    //   },
+    // ],
   },
 };
 
