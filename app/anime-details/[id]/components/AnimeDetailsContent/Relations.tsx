3835c07e345e87c09c9b2a70de1c303fc1eff8be
Jett Dembin <premiumconsulting@jettdembin.dev>
2023-12-11 19:52:01 -0500

    any type added

--- app/anime-details/[id]/components/AnimeDetailsContent/Relations.tsx
+++ app/anime-details/[id]/components/AnimeDetailsContent/Relations.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
-import { Media, Relation } from "@/types/anime";
+import { Media, Relations } from "@/types/anime";
 
 type Props = {
   anime: Media;
-  relations: Relation[];
+  relations: any;
 };
 
 export default function Relations({ anime, relations }: Props) {
@@ -12,7 +12,7 @@ export default function Relations({ anime, relations }: Props) {
     <section>
       <h3 className="text-base mb-2">Relations</h3>
       <div className="flex flex-col space-y-4">
-        {relations?.map((relation, index) => (
+        {relations?.map((relation: any, index: number) => (
           <div
             key={index}
             className="grid bg-white"
