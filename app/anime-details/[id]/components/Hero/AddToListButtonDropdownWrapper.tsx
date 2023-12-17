a196f89bcfe0eb5abbf4f35c74f0211c6b8d8b1e
Jett Dembin <premiumconsulting@jettdembin.dev>
2023-12-17 13:52:54 -0500

    login

--- app/anime-details/[id]/components/Hero/AddToListButtonDropdownWrapper.tsx
+++ app/anime-details/[id]/components/Hero/AddToListButtonDropdownWrapper.tsx
@@ -1,11 +1,44 @@
+"use client";
+
 import { BookmarkIcon } from "@radix-ui/react-icons";
+import { useUser } from "@clerk/nextjs";
 
 import AddToListForm from "@/ui/Card/AnimeCard/Modal/AddToListForm";
+
 import Modal from "@/ui/Modal";
+import LoginWrapper from "@/ui/LoginWrapper";
 
 type Props = {};
 
 export default function AddToListButtonWrapper({}: Props) {
+  const { isSignedIn } = useUser();
+
+  const addToListButton = (
+    <button
+      tabIndex={0}
+      className="flex items-center w-full p-2 bg-blue-200 rounded-sm cursor-pointer relative"
+      onClick={() => {
+        if (isSignedIn) {
+          showModal();
+        }
+      }}
+    >
+      <div className="w-full flex items-center justify-end py-2">
+        <span className="absolute inset-0 flex justify-center items-center">
+          Add to List
+        </span>
+        <BookmarkIcon className="relative bottom-[.1px] w-2 h-2" />
+      </div>
+    </button>
+  );
+
+  const isSignedInAddToListButton = () =>
+    isSignedIn ? (
+      addToListButton
+    ) : (
+      <LoginWrapper signIn>{addToListButton}</LoginWrapper>
+    );
+
   const showModal = () => {
     const modalElement = document.getElementById(
       "add_to_list_modal"
@@ -17,27 +50,8 @@ export default function AddToListButtonWrapper({}: Props) {
 
   return (
     <>
-      <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
-        <button
-          tabIndex={0}
-          className="flex items-center w-full p-2 bg-blue-200 rounded-sm cursor-pointer relative"
-          onClick={() => {
-            showModal();
-          }}
-        >
-          <div className="w-full flex items-center justify-end py-2">
-            <span className="absolute inset-0 flex justify-center items-center">
-              Add to List
-            </span>
-            <BookmarkIcon
-              className="relative bottom-[.1px] w-2 h-2"
-              onClick={(e) => {
-                e.stopPropagation(); // Prevents the modal from opening twice
-                showModal();
-              }}
-            />
-          </div>
-        </button>
+      <div className="">
+        {isSignedInAddToListButton()}
 
         <Modal id="add_to_list_modal">
           <AddToListForm />
