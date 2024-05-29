e94e1948904fdd9880a02710c2352c0c6ec78941
Jett Dembin <jd.dev96@gmail.com>
2024-05-28 20:03:37 -0400

    added back play trailer button to listcard type on all sizes

--- ui/Card/AnimeCard/DescriptiveTypeRefactored.jsx
+++ ui/Card/AnimeCard/DescriptiveTypeRefactored.jsx
@@ -391,7 +391,7 @@ const DescriptiveTypeRefactored = ({ media, isCardHovered }) => {
               {/* <AnimeHoverOptions /> */}
               {!!id && site === "youtube" && (
                 <button
-                  className="p-2 md:p-3 md:hidden bg-white rounded-full cursor-pointer shadow-md mr-1 md:mr-2"
+                  className="p-2 md:p-3  bg-white rounded-full cursor-pointer shadow-md mr-1 md:mr-2"
                   onClick={() => {
                     handleTrailerClick();
                   }}
