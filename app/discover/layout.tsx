7ac6fc1e5f7ffb39421648da86682a0015165770
Jett Dembin <jd.dev96@gmail.com>
2024-08-27 12:09:14 -0400

    fixed discover margins to increase card width

--- app/discover/layout.tsx
+++ app/discover/layout.tsx
@@ -9,11 +9,13 @@ export default function DiscoverLayout({
 }) {
   return (
     <div>
-      <header className="max-w-6xl md:mx-14 lg:mx-auto lg:max-w-4xl xl:max-w-screen-lg relative mt-6">
+      <header className="">
+        {/* <header className="max-w-6xl md:mx-14 lg:mx-auto lg:max-w-4xl xl:max-w-screen-lg relative mt-6"> */}
         <Search />
       </header>
 
-      <main className="max-w-6xl md:mx-14 lg:mx-auto  lg:max-w-4xl xl:max-w-screen-lg relative">
+      <main className="container max-w-7xl mx-auto md:px-4 lg:px-6 xl:px-0">
+        {/* <main className="max-w-6xl md:mx-14 lg:mx-auto  lg:max-w-4xl xl:max-w-screen-lg relative"> */}
         <section className="mt-6 mx-2 md:mx-0">{children}</section>
       </main>
     </div>
